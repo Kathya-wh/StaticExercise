d5d73364e950f12ab7ba3e8bfde86e2fa00e28f3
Kathya <kathyawhite00@gmail.com>
2020-07-02 13:14:02 -0500

    Completed StaticExercise

---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+           var result = (fahrenheit - 32) / 1.8;
+
+            return result;
+        }
+
+        public static double CelsiusToFahrenheit(double Celsius)
+        {
+
+            return (Celsius * 9) / 5 + 32;
+        }
+
+
+
+
+
+
+    }
+}
